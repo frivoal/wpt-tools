d4cfb9155ee99cccc38c563d1323b37f0a240561
kwkbtr <kwkbtr@vivliostyle.com>
2015-05-18 19:15:44 +0900

    Adjust local paths so that wpt-tools can be placed in a directory with a name other than 'tools'

--- localpaths.py
+++ localpaths.py
@@ -2,10 +2,10 @@ import os
 import sys
 
 here = os.path.abspath(os.path.split(__file__)[0])
-repo_root = os.path.abspath(os.path.join(here, os.pardir))
+repo_root = os.path.abspath(here)
 
-sys.path.insert(0, os.path.join(repo_root, "tools"))
-sys.path.insert(0, os.path.join(repo_root, "tools", "six"))
-sys.path.insert(0, os.path.join(repo_root, "tools", "html5lib"))
-sys.path.insert(0, os.path.join(repo_root, "tools", "wptserve"))
-sys.path.insert(0, os.path.join(repo_root, "tools", "pywebsocket", "src"))
+sys.path.insert(0, repo_root)
+sys.path.insert(0, os.path.join(repo_root, "six"))
+sys.path.insert(0, os.path.join(repo_root, "html5lib"))
+sys.path.insert(0, os.path.join(repo_root, "wptserve"))
+sys.path.insert(0, os.path.join(repo_root, "pywebsocket", "src"))
